922522b959daa4332ba2c88add44095fb79cd321
Gitesh Koli <gkoli@google.com>
2020-04-01 10:50:14 -0400

    Update permissions for symlink

--- container-assets/setup-distribution.sh
+++ container-assets/setup-distribution.sh
@@ -6,14 +6,14 @@ if [ ! -d /app/code/vendor ]; then
     find /app/code -mindepth 2 -type d -name .git | xargs rm -rf
 fi
 
-cp /app/container-assets/drush.sh /usr/local/bin/drush && chmod 775 /usr/local/bin/drush
+cp /app/container-assets/drush.sh /usr/local/bin/drush && chmod 755 /usr/local/bin/drush
 
 # Use the settings file which has been parameterized using Environment variables
 cp -f /app/container-assets/parameterized.settings.php /app/code/web/sites/default/settings.php
 
 # fix permissions
 chown -R www-data.www-data /app/code/web/sites/default/settings.php \
-    && chmod -R 755 /app/code/web/sites/default/settings.php
+    && chmod -R 664 /app/code/web/sites/default/settings.php
 
 ln -sf drupal-files/public code/web/sites/default/files
 
@@ -23,8 +23,10 @@ mkdir -p /app/drupal-files/public \
     && mkdir -p /app/drupal-files/temp
 
 chown -R www-data.www-data  /app/drupal-files/public \
-    && chmod -R 755  /app/drupal-files/public \
+    && chmod -R 664  /app/drupal-files/public \
+    && chown -R www-data.www-data /app/code/web/sites/default/files \
+    && chmod -R 664 /app/code/web/sites/default/files \
     && chown -R www-data.www-data /app/drupal-files/private \
-    && chmod -R 755 /app/drupal-files/private \
+    && chmod -R 664 /app/drupal-files/private \
     && chown -R www-data.www-data /app/drupal-files/temp \
-    && chmod -R 755 /app/drupal-files/temp
+    && chmod -R 664 /app/drupal-files/temp
