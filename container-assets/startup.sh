ad6cdf73f90603ceb06a43d8fc7b53904fc25d25
Gitesh Koli <gkoli@google.com>
2020-04-02 22:15:16 -0400

    Fix file permission for public folder

--- container-assets/startup.sh
+++ container-assets/startup.sh
@@ -2,6 +2,6 @@
 
 set -xe
 
-chown -R www-data:www-data /app && chmod -R 775 /app/drupal-files
+chown -R www-data:www-data /app && chmod -R 775 /app/drupal-files /app/code/web/sites/default/files
 
 /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
