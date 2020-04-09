da5515e3d41b0b264893cd3dd56132dda54046e4
Gitesh Koli <gkoli@google.com>
2020-04-08 22:22:05 -0400

    Fix file permissions

--- container-assets/startup.sh
+++ container-assets/startup.sh
@@ -3,7 +3,7 @@
 set -xe
 
 chown -R www-data:www-data /app/drupal-files /app/code/web/sites/default/files
-chmod -R 770 /app/drupal-files /app/code/web/sites/default/files
+chmod -R 775 /app/drupal-files /app/code/web/sites/default/files
 
 # chmod 770 /app/code/web/sites/default/settings.php
 
