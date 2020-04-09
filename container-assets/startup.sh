e6ab9e41489aa2455a2fbc947c4963cd6bff7f50
Gitesh Koli <gkoli@google.com>
2020-04-09 15:11:22 -0400

    Skip lockdown of document root from the base image. We handle it in our scripts. This is to reduce the startup time.

--- container-assets/startup.sh
+++ container-assets/startup.sh
@@ -2,11 +2,13 @@
 
 set -xe
 
-if [ ! "${LOCAL_SETUP}" = "true" ]; then
+if [ "${LOCAL_SETUP}" = "true" ]; then
+  chmod -R 777 /app/drupal-files /app/code/web/sites/default/files
+else
   chown -R www-data:www-data /app/drupal-files /app/code/web/sites/default/files
+  chmod -R 770 /app/drupal-files /app/code/web/sites/default/files
 fi
 
-chmod -R 775 /app/drupal-files /app/code/web/sites/default/files
 
 # chmod 770 /app/code/web/sites/default/settings.php
 
