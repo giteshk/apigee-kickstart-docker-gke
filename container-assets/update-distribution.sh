bc8458ea37dd24fcee9a6f4017d211e60ef6b8c0
Gitesh Koli <gkoli@google.com>
2020-04-09 15:33:30 -0400

    Moved permission to be set after folder creation

--- container-assets/update-distribution.sh
+++ container-assets/update-distribution.sh
@@ -8,6 +8,12 @@ cp /app/container-assets/drush.sh /usr/local/bin/drush && chmod 755 /usr/local/b
 # Use the settings file which has been parameterized using Environment variables
 cp -f /app/container-assets/parameterized.settings.php /app/code/web/sites/default/settings.php
 
+#create the file system for public, private, temp files
+mkdir -p /app/code/web/sites/default/files \
+    && mkdir -p /app/drupal-files/private \
+    && mkdir -p /app/drupal-files/config \
+    && mkdir -p /app/drupal-files/temp
+
 # fix permissions
 if [ "${LOCAL_SETUP}" = "true" ]; then
   chmod -R 777 /app/
@@ -15,9 +21,3 @@ else
   chown -R www-data:www-data /app/ \
       && chmod -R 550 /app/
 fi
-
-#create the file system for public, private, temp files
-mkdir -p /app/code/web/sites/default/files \
-    && mkdir -p /app/drupal-files/private \
-    && mkdir -p /app/drupal-files/config \
-    && mkdir -p /app/drupal-files/temp
