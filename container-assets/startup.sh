0fe466f9685eea1f1bb66f15c01b273706b0e627
Gitesh Koli <gkoli@google.com>
2020-04-10 02:52:10 -0400

    Change permission for the settings.php file

--- container-assets/startup.sh
+++ container-assets/startup.sh
@@ -2,14 +2,15 @@
 
 set -xe
 
+# Use the settings file which has been parameterized using Environment variables
+cp -f /app/container-assets/parameterized.settings.php /app/code/web/sites/default/settings.php
+
 if [ "${LOCAL_SETUP}" = "true" ]; then
   chmod -R 777 /app/drupal-files /app/code/web/sites/default/files
 else
-  chown -R www-data:www-data /app/drupal-files /app/code/web/sites/default/files
-  chmod -R 770 /app/drupal-files /app/code/web/sites/default/files
+  chown -R www-data:www-data /app/drupal-files /app/code/web/sites/default/files /app/code/web/sites/default/settings.php
+  chmod -R 770 /app/drupal-files /app/code/web/sites/default/files /app/code/web/sites/default/settings.php
 fi
 
-# Use the settings file which has been parameterized using Environment variables
-cp -f /app/container-assets/parameterized.settings.php /app/code/web/sites/default/settings.php
 
 /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
